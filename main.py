ed9c719705d9775207bf8bdfb3e7e14924ac7029
BrookLiNN <106794039+AllanPauluk@users.noreply.github.com>
2022-11-24 01:15:53 +0000

    Última Aula (SQLite) Curso de Extensão Python

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from model.Pessoa import Pessoa
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
 
 poyatos = Pessoa(1, "Henrique Poyatos")
+print(poyatos)
 print(poyatos.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO")
+
+db = Database()
+
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+novo = Pessoa(0, "Denzel Washington")
+
+novo = pessoaDAO.save(novo)
+
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
